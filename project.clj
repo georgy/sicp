1369f5004fcbcdd33ca689db70e0613751a671f1
Georgy Bolyuba <bolyuba@gmail.com>
2011-12-04 20:18:55 +0100

    Moved things around, added tests for negative numbers

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject sicp "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "SICP, Clojure way"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
--- test/sicp/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns sicp.test.core
-  (:use [sicp.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
