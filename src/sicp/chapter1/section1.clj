385a4e25f5179e434009656730def7c582e53973
Georgy Bolyuba <bolyuba@gmail.com>
2012-02-22 23:02:11 +0100

    +1.6

--- src/sicp/chapter1/section1.clj
+++ src/sicp/chapter1/section1.clj
@@ -1,6 +1,6 @@
 (ns sicp.chapter1.section1)
 
-; Exercise 1.2 
+;; Exercise 1.2 
 (defn ex12
   []
   (/ (+ 5
@@ -12,9 +12,9 @@
         (- 6 2)
         (- 2 7))))
 
-; Exercise 1.3
-; Define a procedure that takes three numbers as arguments and returns
-; the sum of the squares of the two larger numbers.
+;; Exercise 1.3
+;; Define a procedure that takes three numbers as arguments and returns
+;; the sum of the squares of the two larger numbers.
 
 (defn- pow2
   [x]
@@ -35,7 +35,7 @@
       (sum-of-squares y x)
       (sum-of-squares y z))))
 
-; Exercise 1.4  
+;; Exercise 1.4  
 (defn selects-operator-by-b
   [a b]
   ((if (> b 0) + -) a b))
@@ -52,10 +52,44 @@
     0
     y))
 
-;(testp [] 0 (p))
+;;(testp [] 0 (p))
 
-; This will blow up the stack due to evaluation of (p). This is
-;consistent with applicative-order evaluation. i.e. arguments are
-;evaluated before application 
+;; This will blow up the stack due to evaluation of (p). This is
+;;consistent with applicative-order evaluation. i.e. arguments are
+;;evaluated before application 
 
 
+;; Exercise 1.6
+
+(defn new-if
+  [predicate then-cl else-cl]
+  (cond predicate then-cl
+        :else else-cl))
+
+(defn improve
+  [guess x]
+  (let
+      [avg (fn [x y] (/ ( + x y) 2))]
+    (avg guess (/ x guess))))
+
+(defn abs
+  [x]
+  (cond (< x 0) (- 0 x)
+        :else x))
+
+(defn good-enough?
+  [guess x]
+  (< (abs (- (* guess guess) x)) 0.001))
+
+(defn sqrt-iter
+  [guess x]
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+
+;; Running non-stop, consuming 100% of stack.
+;;
+;;
+;;
